9b87837ec1555a3dc484e357e221498c4fc07fd5
Mario Braham <mario.braham@mymona.uwi.edu>
2017-10-29 23:24:53 +0000

    Excercise 3

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 window.onload=function(){
     let search=document.getElementById("search");
+    let result=document.getElementById("result");
     search.addEventListener("click",function(){
         let request = new XMLHttpRequest();
         request.onreadystatechange = function(){
             if(request.readyState === 4 && request.status === 200){
-                alert(request.responseText);
+                let response=document.createElement("p");
+                response.innerHTML=request.responseText;
+                result.appendChild(response);
             }
         };
-        request.open("GET","request.php?q=definition",true);
+        let url = "request.php?q="+document.getElementById("search_value").value;
+        request.open("GET",url,true);
         request.send();
     });
 };
