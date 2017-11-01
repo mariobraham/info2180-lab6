5a0848fd8c8e314d527a2733a6400977d723dc6f
Mario Braham <mario.braham@mymona.uwi.edu>
2017-11-01 21:27:46 +0000

    Exercise 3 Modified

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
 window.onload=function(){
     let search=document.getElementById("search");
-    let search_all=document.getElementById("definition");
     let result=document.getElementById("result");
-    let search_term = document.getElementById("search_value");
+    let search_term=document.getElementById("search_value");
     search.addEventListener("click",function(){
         let request = new XMLHttpRequest();
         request.onreadystatechange = function(){
             if(request.readyState === 4 && request.status === 200){
-                result.innerHTML = "<h2>Results</h2><p>"+request.responseText+"</p>";
+                result.innerHTML = "<h2>Result</h2>"+request.responseText;
             }
         };
-        let url = "request.php?q=&all=true";
-        request.open("GET","request.php?q=&all=true",true);
+        let url = "request.php?q="+search_term.value;
+        request.open("GET",url,true);
         request.send();
-    });
-    search_all.addEventListener("click",function(){
-        let request_all = new XMLHttpRequest();
-        request_all.onreadystatechange = function(){
-            if(request_all.readyState === 4 && request_all.status === 200){
-                let list="<ol>";
-                let xml_document = request_all.responseXML;
-                let words = xml_document.getElementsByTagName("definition");
-                for(let index = 0; index<words.length; index++){
-                    list+="<li><h3>"+words[index].getAttribute("name")+"</h3><p>"+words[index].childNodes[0].nodeValue+"</p><p> - "+words[index].getAttributeNode("author").value+"</p></li>";
-                }
-                list+="</ol>";
-                result.innerHTML = "<h2>Result</h2>"+list;
-            }
-        };
-        request_all.open("GET","request.php?q=&all=true",true);
-        request_all.send();
     });
 };
--- request.php
+++ request.php
@@ -11,32 +11,5 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-//print "<h3>" . strtoupper($query) . "</h3>";
-//print "<p>" . $definition[$query] . "</p>";
-$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
-<entries>
-    <definition name = "definition" author = "John" >
-        A statement of the exact meaning of a word, especially in a dictionary.
-    </definition>
-    <definition name = "ajax" author = "Kimberly">
-        Technique which involves the use of javascript and xml (or JSON)
-    </definition>
-    <definition name = "html" author = "Tim">
-        The standard markup language for creating web pages and web applications.
-    </definition>
-    <definition name = "css" author = "Racheal">
-        A style sheet language used for describing the presentation of a document written in a markup language.
-    </definition>
-    <definition name = "javascript" author = "Albert">
-        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
-    </definition>
-    <definition name = "php" author = "Isaac">
-        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
-    </definition>
-    <definition name = "bar" author = "Mary">
-        A place that sells alcholic beverages.
-    </definition>
-</entries>';
-header('Content-Type: text/xml');
-$xmlOutput = new SimpleXMLElement($xmldata);
-echo $xmlOutput->asXML();
+print "<h3>" . strtoupper($query) . "</h3>";
+print "<p>" . $definition[$query] . "</p>";
