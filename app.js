38f780f953d211aa0a9539b0fe9249a23319fbf2
Mario Braham <mario.braham@mymona.uwi.edu>
2017-11-01 21:35:55 +0000

    Exercise 4 Modified

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ window.onload=function(){
     let search=document.getElementById("search");
     let result=document.getElementById("result");
     let search_term=document.getElementById("search_value");
+    let search_all=document.getElementById("definition");
     search.addEventListener("click",function(){
         let request = new XMLHttpRequest();
         request.onreadystatechange = function(){
@@ -12,5 +13,22 @@ window.onload=function(){
         let url = "request.php?q="+search_term.value;
         request.open("GET",url,true);
         request.send();
+    });
+    search_all.addEventListener("click",function(){
+        let request_all = new XMLHttpRequest();
+        request_all.onreadystatechange = function(){
+            if(request_all.readyState === 4 && request_all.status === 200){
+                let list="<ol>";
+                let xml_document = request_all.responseXML;
+                let words = xml_document.getElementsByTagName("definition");
+                for(let index = 0; index<words.length; index++){
+                    list+="<li><h3>"+words[index].getAttribute("name")+"</h3><p>"+words[index].childNodes[0].nodeValue+"</p><p> - "+words[index].getAttributeNode("author").value+"</p></li>";
+                }
+                list+="</ol>";
+                result.innerHTML = "<h2>Result</h2>"+list;
+            }
+        };
+        request_all.open("GET","request.php?q=&all=true",true);
+        request_all.send();
     });
 };
--- request.php
+++ request.php
@@ -2,6 +2,7 @@
 // accept a term (keyword)
 // respond with a value
 $query = $_GET['q'];
+$all = $_GET['all'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -11,5 +12,35 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name = "definition" author = "John" >
+        A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    <definition name = "ajax" author = "Kimberly">
+        Technique which involves the use of javascript and xml (or JSON)
+    </definition>
+    <definition name = "html" author = "Tim">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+    <definition name = "css" author = "Racheal">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition name = "javascript" author = "Albert">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name = "php" author = "Isaac">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+    </definition>
+    <definition name = "bar" author = "Mary">
+        A place that sells alcholic beverages.
+    </definition>
+</entries>';
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+if($all){
+    echo $xmlOutput->asXML();
+}else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
